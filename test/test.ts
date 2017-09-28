bc052acf3276053fa5b29259b48e25e1fb8be6e6
Meng Wang <wm@wmpcxpy.com>
2017-09-27 20:10:34 -0500

    use yarn and npm script for windows testing

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,12 @@
 import redog from '../src/ReDog';
 
 let a = new redog(document.getElementById("root"),
-    "<div>\
+    "<div style=background-color:black;color:white>\
     <span>dasd<div>\
-        <span id=12312>{test}</span>1231231\
-        </div>11<span>11</span>123</span></div>"
+        <span id=12312 style=color:red>{test}</span>1231231\
+        </div>11\
+        <span style=color:purple>11</span>123\
+        </span>\
+    </div>"
 );
 a.mount()
