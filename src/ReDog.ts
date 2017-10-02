daecc9c9cb758123ec5adeec500da852cd0aafa6
WMXPY <wm@mengw.io>
2017-10-02 14:22:10 -0500

    update Redog

--- src/ReDog.ts
+++ src/ReDog.ts
@@ -1,25 +1,13 @@
 import vdom from '../dogDOM/dogDOM';
 
 export default class {
-    private rootElement: HTMLElement;
+    private rootElement: any;
     private rootDOM: vdom;
-
-    constructor(DOMElement: HTMLElement, DOM: string) {
-        this.rootElement = DOMElement;
-        this.rootDOM = new vdom(DOM, this);
+    constructor(docelement) {
+        this.rootElement = docelement;
     }
 
-    stringify(): string {
-        return this.rootDOM.renderString();
-    }
-
-    mount(): void {
-        this.rootElement.innerHTML = "";
-        this.rootElement.appendChild(this.rootDOM.render());
-    }
-
-    reRender(): void {
-        this.rootElement.innerHTML = "";
-        this.rootElement.appendChild(this.rootDOM.render());
+    mount() {
+        this.rootElement.innerHTML = this.rootDOM.render();
     }
 }
